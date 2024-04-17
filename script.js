2f5ed2d0d7077ed5ffe268e82b6b8c75db055c79
Sivachandran <141358439+Siva8401@users.noreply.github.com>
2024-04-17 16:08:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,20 +6,20 @@ function searchICCID() {
     return;
   }
   
-  fetch("https://console.mercydatrack.com/m2mportal/devices/iccid/" + iccid)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4) {
+      if (this.status == 200) {
+        var data = JSON.parse(this.responseText);
+        displayResult(data);
+      } else {
+        console.error("Error fetching data:", this.statusText);
+        alert("Error fetching data. Please try again later.");
       }
-      return response.json();
-    })
-    .then(data => {
-      displayResult(data);
-    })
-    .catch(error => {
-      console.error("Error fetching data:", error);
-      alert("Error fetching data. Please try again later.");
-    });
+    }
+  };
+  xhttp.open("GET", "https://console.mercydatrack.com/m2mportal/devices/iccid/" + iccid, true);
+  xhttp.send();
 }
 
 function displayResult(data) {

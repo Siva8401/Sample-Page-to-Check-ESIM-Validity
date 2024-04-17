f633aad42433eb653b7a09a30bae52a644a303bf
Sivachandran <141358439+Siva8401@users.noreply.github.com>
2024-04-17 16:11:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,11 @@ function searchICCID() {
   
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
+    console.log("ReadyState:", this.readyState);
     if (this.readyState == 4) {
+      console.log("Status:", this.status);
       if (this.status == 200) {
+        console.log("Response:", this.responseText);
         var data = JSON.parse(this.responseText);
         displayResult(data);
       } else {
@@ -20,27 +23,4 @@ function searchICCID() {
   };
   xhttp.open("GET", "https://console.mercydatrack.com/m2mportal/devices/iccid/" + iccid, true);
   xhttp.send();
-}
-
-function displayResult(data) {
-  var resultContainer = document.getElementById("resultContainer");
-  var resultElement = document.getElementById("result");
-  
-  resultElement.innerHTML = ""; // Clear previous results
-  
-  if (data.status === "success") {
-    resultElement.innerHTML += "<p>Primary TSP MSISDN: " + data.data.Primary_TSP_MSISDN + "</p>";
-    resultElement.innerHTML += "<p>Fallback TSP MSISDN: " + data.data.Fallback_TSP_MSISDN + "</p>";
-    resultElement.innerHTML += "<p>Primary TSP Name: " + data.data.Primary_TSP_Name + "</p>";
-    resultElement.innerHTML += "<p>Fallback TSP Name: " + data.data.Fallback_TSP_Name + "</p>";
-    resultElement.innerHTML += "<p>Primary TSP Validity: " + data.data.Primary_TSP_Validity + "</p>";
-    resultElement.innerHTML += "<p>Fallback TSP Validity: " + data.data.Fallback_TSP_Validity + "</p>";
-    resultElement.innerHTML += "<p>Primary Status: " + data.data.PrimaryStatus + "</p>";
-    resultElement.innerHTML += "<p>Fallback Status: " + data.data.FallbackStatus + "</p>";
-    resultElement.innerHTML += "<p>IMEI No: " + data.data.ImeiNo + "</p>";
-    resultContainer.style.display = "block"; // Show result container
-  } else {
-    resultElement.innerHTML = "<p>No data found for the given ICCID.</p>";
-    resultContainer.style.display = "none"; // Hide result container
-  }
 }
